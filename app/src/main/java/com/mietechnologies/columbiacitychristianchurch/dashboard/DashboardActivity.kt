0d382daf3a701457c4dcbb83dad92513fdbb55cd
Michael Craun <michael.craun@gmail.com>
2024-06-23 22:02:31 -0400

    Add Jetpack Compose support

---
+++ app/src/main/java/com/mietechnologies/columbiacitychristianchurch/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.mietechnologies.columbiacitychristianchurch.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mietechnologies.columbiacitychristianchurch.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
