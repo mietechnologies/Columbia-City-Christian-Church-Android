3a646fe0e46440a6698b0819a8ed11d1373f86a4
Michael Craun <michael.craun@gmail.com>
2024-06-24 10:41:10 -0400

    WIP: Setting up ScrollingNavigationBar

---
+++ app/src/main/java/com/mietechnologies/columbiacitychristianchurch/dashboard/AppTab.kt
@@ -0,0 +1,10 @@
+package com.mietechnologies.columbiacitychristianchurch.dashboard
+
+enum class AppTab(val title: String) {
+    DASHBOARD("Dashboard"),
+    CHECK_IN("Check In"),
+    TITHE("Tithe"),
+    SERMON_NOTES("Sermon Notes"),
+    PRAYER_CHAIN("Prayer Chain"),
+    EVENTS("Events")
+}
