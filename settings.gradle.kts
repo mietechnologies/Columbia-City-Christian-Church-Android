b3c29e2c54d5f987f32900fe8721fc5d62818867
Michael Craun <michael.craun@gmail.com>
2024-06-23 21:53:03 -0400

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Columbia City Christian Church"
+include(":app")
+ 
