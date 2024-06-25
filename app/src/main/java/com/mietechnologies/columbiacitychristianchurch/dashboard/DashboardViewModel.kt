b33e137f586e71b80f339719c42d83ffcd363a68
Michael Craun <michael.craun@gmail.com>
2024-06-25 10:37:15 -0400

    Minor UI improvements
    - NavigationBar now uses AppText for the title
    - MapView now requires a DashboardViewModel for delivery of longitude and latitude so these values can be delivered from the backend
    - Location latitude and longitude are now stored in DashboardViewModel

--- app/src/main/java/com/mietechnologies/columbiacitychristianchurch/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/mietechnologies/columbiacitychristianchurch/dashboard/DashboardViewModel.kt
@@ -5,10 +5,14 @@ import androidx.lifecycle.ViewModel
 
 class DashboardViewModel: ViewModel() {
     val introduction = mutableStateOf("We welcome you to worship Jesus with us as we gather in the presence of fellow believers. Service starts at 10 am or arrive early to enjoy coffee and donuts with us!")
+
+    // MARK: - Location
     val locationName = mutableStateOf("Columbia City Christian Church")
     val locationStreet = mutableStateOf("270 Frontage Rd.")
     val locationCity = mutableStateOf("Columbia City")
     val locationState = mutableStateOf("IN")
     val locationZip = mutableStateOf("46725")
     val locationPhone = mutableStateOf("2602446709")
+    val locationLat = mutableStateOf(41.170451537819794)
+    val locationLng = mutableStateOf(-85.49108881611338)
 }
